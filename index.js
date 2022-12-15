89e9c6aec357cb29d2475ae486c762369f027673
Dan Rees <danrees8743@gmail.com>
2022-12-15 10:14:34 +0000

    feat[main]: Solved all problems on the challenges set by David

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv');
+const charactersRouter = require('./routes/characters.js');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//* Middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(colors.yellow.italic(req.path, req.method));
+
+  next();
+});
+
+//* Routes
+app.use('/', charactersRouter);
+
+app.listen(PORT, () => {
+  console.log(colors.cyan.italic(`Server is running on port ${PORT}`));
+  console.log();
+});
---
+++ routes/characters.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const characters = require('../characters.json');
+const colors = require('colors');
+
+const charactersRouter = express.Router();
+
+//* Gets all the character and filters by blood
+charactersRouter.get('/characters', (req, res) => {
+  const { query } = req;
+  const { blood } = query;
+  if (blood) {
+    const filteredCharacters = characters.filter((character) =>
+      character.blood.toLowerCase().includes(blood.toLowerCase())
+    );
+    if (!filteredCharacters.length)
+      return res
+        .status(404)
+        .json({ message: 'No character was found, Please try another' });
+    return res.status(200).json(filteredCharacters);
+  }
+
+  res.status(200).json(characters);
+});
+
+//* Get a single character by id
+charactersRouter.get('/characters/:id', (req, res) => {
+  const { id } = req.params;
+  const character = characters.find((character) => character.id === Number(id));
+  if (!character)
+    return res
+      .status(404)
+      .json({ message: 'No character was found, Please try another' });
+  res.status(200).json(character);
+});
+
+//* Gets characters on from their birth month
+charactersRouter.get('/characters/birth/:month', (req, res) => {
+  const { month } = req.params;
+  const charactersByMonth = characters.filter((character) => {
+    const characterBirthMonth =
+      character.born.toLowerCase().includes(month) ||
+      character.born.includes(month);
+    return characterBirthMonth;
+  });
+  if (!charactersByMonth.length)
+    return res
+      .status(404)
+      .json({ message: 'No character was found, Please try another' });
+  res.status(200).json(charactersByMonth);
+});
+
+//* Gets the character for the Quidditch team and referees
+charactersRouter.get('/characters/quidditch/match', (req, res) => {
+  const groupSize = 6;
+  const group_1 = [];
+  const group_2 = [];
+  const group_3 = [];
+  const group_4 = [];
+  const referee = [];
+
+  //* Group size is six,  Teams are the players divided by four
+  for (let i = 1; i <= groupSize; i++) {
+    group_1.push(characters[i]);
+    group_2.push(characters[i + 1]);
+    group_3.push(characters[i + 2]);
+    group_4.push(characters[i + 3]);
+
+    //* Referees are who are not in the team
+    if (group_1.length === groupSize) {
+      referee.push(characters[i + 4]);
+    }
+  }
+
+  //* Two matches with two groups
+
+  const matches = {
+    match1: ['Group 1:', ...group_1, 'Group 2:', ...group_2],
+    match2: ['Group 3:', ...group_3, 'Group 4:', ...group_4],
+    referees: ['Referee:', ...referee],
+  };
+
+  res.status(200).json({ matches });
+});
+
+module.exports = charactersRouter;
